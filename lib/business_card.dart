48db8736c64d2e58beed4ae42f97325da2243fc9
GVux <62868382+GVux@users.noreply.github.com>
2022-02-08 14:58:02 -0800

    9

--- lib/business_card.dart
+++ lib/business_card.dart
@@ -11,15 +11,20 @@ class MainScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Center(child: Text(title))),
         //body: CenteredPlaceholder(padding: 50));
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            SizedBox(child: Placeholder(), width: 100, height: 100),
-            SizedBox(child: Placeholder(), width: 100, height: 100)
-          ]),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            SizedBox(child: Placeholder(), width: 100, height: 100),
-            SizedBox(child: Placeholder(), width: 100, height: 100)
-          ])
-        ]));
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [placeholderRow(), placeholderRow()]));
+  }
+
+  Widget paddedPlaceholder() {
+    return Padding(
+        padding: EdgeInsets.all(20),
+        child: SizedBox(child: Placeholder(), width: 100, height: 100));
+  }
+
+  Widget placeholderRow() {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [paddedPlaceholder(), paddedPlaceholder()]);
   }
 }
